e61d539cb53af2e8a77dddee6d6ab0c38ef126e7
camila stano <camilastano.f@gmail.com>
2021-02-18 15:26:33 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+base_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks = JSON.parse(open(base_url).read).first[1]
+
+puts 'Cleaning up database...'
+Ingredient.destroy_all
+puts "Database is clean!"
+
+
+puts 'Creating Ingredients'
+(0..50).to_a.each do |index|
+  ingredient = Ingredient.create(
+    name: drinks[index].values.first
+  )
+  puts "Ingredient #{ingredient.id} is created."
+end
+
+puts 'All Done!'
