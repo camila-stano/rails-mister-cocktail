eab7042bac2f0cc7bd7f684a1606d51db9bee08c
camila stano <camilastano.f@gmail.com>
2021-04-03 20:01:17 -0300

    Base modal  para novo comentário

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,13 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-import 'bootstrap';
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+var jQuery = require('jquery')
+global.$ = global.jQuery  = jQuery;
+window.$ = window.jQuery = jQuery;
+
+// External imports
+import "bootstrap";
+
+// import "controllers";
